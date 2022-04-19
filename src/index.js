bdf58d24c70bcec91a60860f4fdb4b6aa0c8d9b0
Jeroen van Riel <jeroenvanriel@outlook.com>
2022-04-19 21:44:23 +0200

    simple tree model viewer

--- src/index.js
+++ src/index.js
@@ -1,24 +1,41 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import model from './model.glb';
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.outputEncoding = THREE.sRGBEncoding;
+document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
-document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(10, 10, 10);
+controls.target.set(0, 8, 0);
+controls.update()
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+const scene = new THREE.Scene();
+
+const loader = new GLTFLoader();
+loader.load(model, function(gltf) {
+  scene.add(gltf.scene);
+}, undefined, function(error) {
+  console.error(error);
+});
+
+const light = new THREE.AmbientLight(0x404040);
+scene.add(light);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
 
 function animate() {
-  requestAnimationFrame( animate );
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.glb$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
